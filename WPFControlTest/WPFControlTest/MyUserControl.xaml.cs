ea2da354f1f6528c68237018e409c044763556cb
Saltylanguage <joshcampbell1986@gmail.com>
2016-12-18 16:26:49 -0800

    Josh: Updated - Color Picker, extended functionality Added - Value Binding for RGB values

--- WPFControlTest/WPFControlTest/MyUserControl.xaml.cs
+++ WPFControlTest/WPFControlTest/MyUserControl.xaml.cs
@@ -25,9 +25,63 @@ namespace WPFControlTest
             InitializeComponent();
         }
 
-        private void colorPicker_1_LostFocus(object sender, RoutedEventArgs e)
-        {           
-            colorPicker_1.Background = new SolidColorBrush(colorPicker_1.SelectedColor.GetValueOrDefault());
+        private void colorPicker_1_ValueChanged(object sender, RoutedEventArgs e)
+        {
+
+            Color adjustedColor = colorPicker_1.SelectedColor.GetValueOrDefault();
+            colorPicker_1.Background = new SolidColorBrush(adjustedColor);
+            Redvalue.Text = adjustedColor.R.ToString();
+            GreenValue.Text = adjustedColor.G.ToString();
+            BlueValue.Text = adjustedColor.B.ToString();
+            AlphaValue.Text = adjustedColor.A.ToString();
+            //colorPicker_1.Background = new SolidColorBrush(colorPicker_1.SelectedColor.GetValueOrDefault());
+
+            //Redvalue.Text
+        }
+
+        private void RedValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            Color myColor = colorPicker_1.SelectedColor.GetValueOrDefault();
+
+            if (Redvalue.Text != null)
+            {
+                myColor.R = Convert.ToByte(Redvalue.Text);
+                colorPicker_1.SelectedColor = myColor;
+            }
+        }
+
+        private void GreenValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Color myColor = colorPicker_1.SelectedColor.GetValueOrDefault();
+
+            if (Redvalue.Text != null)
+            {
+                myColor.G = Convert.ToByte(GreenValue.Text);
+                colorPicker_1.SelectedColor = myColor;
+            }
+        }
+
+        private void BlueValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Color myColor = colorPicker_1.SelectedColor.GetValueOrDefault();
+
+            if (Redvalue.Text != null)
+            {
+                myColor.B = Convert.ToByte(BlueValue.Text);
+                colorPicker_1.SelectedColor = myColor;
+            }
+        }
+
+        private void AlphaValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Color myColor = colorPicker_1.SelectedColor.GetValueOrDefault();
+
+            if (Redvalue.Text != null)
+            {
+                myColor.A = Convert.ToByte(AlphaValue.Text);
+                colorPicker_1.SelectedColor = myColor;
+            }
         }
     }
 }
